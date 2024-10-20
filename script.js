958ba3eed454dcca55c2c0bf4d629053d4584052
owleyeview <rashaanlightpool@gmail.com>
2024-10-20 16:17:48 -0700

    first draft complete

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.getElementById("hamburger-menu");
     const navLinks = document.getElementById("nav-links");
+    const contactForm = document.getElementById("travel-agent-form");
+    const contactModal = document.getElementById("contact-modal");
 
     hamburger.addEventListener("click", function () {
         navLinks.classList.toggle("show");
     });
+
+    if (contactForm) {
+        contactForm.addEventListener("submit", function (e) {
+            e.preventDefault();
+            contactModal.style.display = "block";
+            
+            setTimeout(function () {
+                window.location.href = "index.html";
+            }, 2000); // Redirect after 2 seconds
+        });
+    }
 });
